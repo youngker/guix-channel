f1626c8caad010d552642863bce5dbfdab97d5f6
YoungJoo Lee <youngker@gmail.com>
2020-12-05 19:43:11 +0900

    test

--- youngker/packages/myst.scm
+++ youngker/packages/myst.scm
@@ -1,23 +1,22 @@
-(define-module (guix packages myst)
+(define-module (youngker packages myst)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu))
-;;  #:use-module (gnu packages myst))
 
 (define-public myst
   (package
    (name "myst")
    (version "0.1")
-   (source 
+   (source
     (origin
      (method git-fetch)
-     (uri 
+     (uri
       (git-reference
        (url "https://github.com/LukeSmithxyz/st.git")
        (commit "13b3c631be13849cd80bef76ada7ead93ad48dc6")))
      (file-name (git-file-name name version))
-     (sha256 
+     (sha256
       (base32 "009za6dv8cr2brs31sjqixnkk3jwm8k62qk38sz4ggby3ps9dzf4"))))
 
    (arguments
@@ -26,8 +25,7 @@
       (modify-phases %standard-phases
                      (delete 'configure))))
    (build-system gnu-build-system)
-   (home-page "ttt")
-   (synopsis "ttt")
-   (description "ttt")
+   (home-page "tbd")
+   (synopsis "tbd")
+   (description "tbd")
    (license license:gpl3+)))
-
